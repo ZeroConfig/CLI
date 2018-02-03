440b3837bec101da631a14423a08269971ed4572
Jan-Marten de Boer <github@johmanx.com>
2018-02-03 22:26:58 +0100

    Only support HTTP or HTTPS URLs.

--- src/Reader/HttpResource.php
+++ src/Reader/HttpResource.php
@@ -5,6 +5,8 @@
  */
 namespace ZeroConfig\Cli\Reader;
 
+use InvalidArgumentException;
+
 class HttpResource extends AbstractReader
 {
     /** @var string */
@@ -14,9 +16,23 @@ class HttpResource extends AbstractReader
      * Constructor.
      *
      * @param string $url
+     *
+     * @throws InvalidArgumentException When the URL scheme is not http or https.
      */
     public function __construct(string $url)
     {
+        $scheme = parse_url($url, PHP_URL_SCHEME);
+
+        if (!in_array($scheme, ['http', 'https'], true)) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    'Invalid URL "%s" with scheme "%s" supplied.',
+                    $url,
+                    $scheme
+                )
+            );
+        }
+
         $this->url = $url;
     }
 
